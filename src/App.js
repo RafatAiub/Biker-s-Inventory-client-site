fce9e94adb3b20360989d335da91c853a1139a41
RafatAiub <tanvirmahtab1996@gmail.com>
2022-05-07 10:56:02 +0600

    header banner footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Banner from './HomePage/Banner';
+import Footer from './HomePage/Footer';
+import Header from './HomePage/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Inventory Web</h1>
+      <Header></Header>
+      <Banner></Banner>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/HomePage/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-black'>
+            <div className='content'>
+                <p>Developed By: Tanvir Mahtab Rafat</p>
+                <p><a href="https://www.facebook.com/tanvirmahtab.rafat">tanvirmahtab1996@gmail.com</a></p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
