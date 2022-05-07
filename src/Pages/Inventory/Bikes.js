89c5b2547bec955617d8d89c3e01fba0ab855a3b
RafatAiub <tanvirmahtab1996@gmail.com>
2022-05-07 17:35:36 +0600

    connect with mongo bd server with 5000 port

--- src/Pages/Inventory/Bikes.js
+++ src/Pages/Inventory/Bikes.js
@@ -5,7 +5,7 @@ import './Bikes.css'
 const Bikes = () => {
     const [bikes, setBikes] = useState([]);
     useEffect(() => {
-        fetch('bikes.json')
+        fetch('http://localhost:5000/bikes')
             .then(res => res.json())
             .then(data => setBikes(data));
 
